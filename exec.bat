bc48985ce874032ebb53dbacdac2f4be7bf130ca
Tlospock <meunierpaul@free.fr>
2017-11-24 19:23:53 +0100

    modification of batch file to create a png for each file

--- exec.bat
+++ exec.bat
@@ -1,5 +1,8 @@
 REM Batch file to launch ray tracer on specified file
+REM change the input_file value to change file
 set input_file=scene02.yaml
 echo input_file
 bin\debug\GraphicsRayTracer.exe %input_file% output.png
+bin\debug\GraphicsRayTracer.exe scene01.yaml scene01.png
+bin\debug\GraphicsRayTracer.exe scene02.yaml scene02.png
 pause
