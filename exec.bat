b30b5b3ebe8cebc64e4b01e177881b1fbdce07a5
Tlospock <meunierpaul@free.fr>
2017-11-24 19:32:39 +0100

    added a readme and modified exec.bat file to open output.png after execution

--- exec.bat
+++ exec.bat
@@ -3,6 +3,10 @@ REM change the input_file value to change file
 set input_file=scene02.yaml
 echo input_file
 bin\debug\GraphicsRayTracer.exe %input_file% output.png
+
 bin\debug\GraphicsRayTracer.exe scene01.yaml scene01.png
 bin\debug\GraphicsRayTracer.exe scene02.yaml scene02.png
+
+REM Open the output file
+output.png
 pause
