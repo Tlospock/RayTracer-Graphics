012f3e3fdfbaa5a9926d6f4dca1fd824872e638f
Tlospock <meunierpaul@free.fr>
2017-12-16 19:30:19 +0100

    Classe camera

--- camera.cpp
+++ camera.cpp
@@ -1,1 +1,7 @@
+#include "Camera.h"
+#include <iostream>
+#include <math.h>
 
+Camera::Camera(Triple eye, Triple center, Triple up, Vector viewSize) : eye(eye), center(center), up(up), viewSize(viewSize){
+
+}
--- camera.h
+++ camera.h
@@ -1,1 +1,17 @@
+#pragma once
+#include "object.h"
+#include "triple.h"
+
+class Camera :
+	public Object
+{
+public:
+	Camera(Triple eye, Triple center, Triple up, Vector viewSize);
+
+private:
+	Triple eye;
+	Triple center;
+	Triple up;
+	Vector viewSize;
+};
 
